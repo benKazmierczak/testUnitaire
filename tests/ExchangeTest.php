5f1b426b9cb00fbfd31d0ef2afc86b18ef27e438
benjamin <kazmierczakbenjamin@gmail.com>
2019-05-05 21:50:16 +0200

    ajout du config

---
+++ tests/ExchangeTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin
+ * Date: 2019-04-26
+ * Time: 17:07
+ */
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\User;
+use App\Entity\Product;
+use App\Entity\Exchange;
+
+/*class ExchangeTest extends TestCase
+
+}*/
