63e1f656a551cafe8d1e190af17f8f4ea4f18c03
Domange <backinsmail@gmail.com>
2019-05-06 23:06:26 +0200

    DBConnection

--- src/Entity/DBConnection.php
+++ src/Entity/DBConnection.php
@@ -1,21 +1,57 @@
 <?php
 
+namespace App\Entity;
+
+use App\Entity\Product;
+use App\Entity\User;
+use App\Entity\Exchange;
+use PHPUnit\Runner\Exception;
+
 class DBConnection
 {
 
-	function __construct()
+    /**
+     * @var Product
+     */
+    private $product;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var Exchange
+     */
+    private $exchange;
+
+	function __construct(Product $product, User $user, Exchange $exchange)
 	{
-		//
+	    $this->setProduct($product);
+	    $this->setUser($user);
+        $this->setExchange($exchange);
 	}
 
-	public function saveProduct($product)
+    /**
+     * @return bool
+     */
+	public function saveProduct()
 	{
-		throw new Exception('Not implemented');
+	    if($this->product instanceof Product && $this->product->isValid()) {
+	        return true;
+        }
+        throw new Exception('Code error: 1');
 	}
 
-	public function saveUser($user)
+    /**
+     * @return bool
+     */
+	public function saveUser()
 	{
-		throw new Exception('Not implemented');
+        if($this->user instanceof Product && $this->user->isValid()) {
+            return true;
+        }
+        throw new Exception('Code error: 2');
 	}
 
 	public function saveExchange($exchange)
@@ -23,4 +59,51 @@ class DBConnection
 		throw new Exception('Not implemented');
 	}
 
+    /**
+     * @param Product $product
+     */
+	public function setProduct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @param User $user
+     */
+	public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @param Exchange $exchange
+     */
+	public function setExchange(Exchange $exchange)
+    {
+        $this->exchange = $exchange;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct(): \App\Entity\Product
+    {
+        return $this->product;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): \App\Entity\User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return Exchange
+     */
+    public function getExchange(): \App\Entity\Exchange
+    {
+        return $this->exchange;
+    }
 }
